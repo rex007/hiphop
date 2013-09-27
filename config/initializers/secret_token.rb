8ca8a40c53c7dd6a67c3f1d30f64d313cef1df23
Andraz Kuret <rex007@gmail.com>
2013-09-27 20:36:18 +0200

    DB to psql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hiphop::Application.config.secret_key_base = '7c55d14bc2c405a045e535dba865e09bb0453b0e2b38a9663d44e42a95a054786eea5a3fa0b708bac7806614b404a2a4f39d7540c909a86681fd589349c2d8ca'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Hiphop::Application.config.session_store :cookie_store, key: '_hiphop_session'
